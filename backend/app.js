f677945531f46ad4576ab0ebdfd2115c5a9ac032
Niraj Gurung <grgniraj01@gmail.com>
2025-09-03 21:49:45 +0545

    Deployment updates

--- backend/app.js
+++ backend/app.js
@@ -32,14 +32,6 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
-app.options(
-  "*",
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  })
-);
 app.use(express.json());
 
 app.use(cookieParser());
